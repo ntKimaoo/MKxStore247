4316788ba3cd9af1ee58ccc20fc1be3fd0817abb
ntKimaoo <nhukien0603@gmail.com>
2025-06-10 10:58:40 +0700

    test2

--- MKxStore247/obj/Debug/net8.0/MKxStore247.AssemblyInfo.cs
+++ MKxStore247/obj/Debug/net8.0/MKxStore247.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MKxStore247")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea30b916ae0ac1eacaa1085db41374997eec5fa3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9ff09cb58e438f5e30300e556b241de079584ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("MKxStore247")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MKxStore247")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
