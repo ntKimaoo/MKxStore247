6769b39f4cd7e3eb2fab5d201bf7743c3bd6fc61
ntKimaoo <nhukien0603@gmail.com>
2025-06-10 11:10:49 +0700

    Sửa ProductImage.cs từ nhánh kien

--- MKxStore247/Models/ProductImage.cs
+++ MKxStore247/Models/ProductImage.cs
@@ -10,7 +10,7 @@ namespace MKxStore247.Models
 
         [Required]
         public int ProductId { get; set; }
-
+        public string FileName => "From Kien branch";
         [Required]
         [MaxLength(255)]
         public string ImageUrl { get; set; }
